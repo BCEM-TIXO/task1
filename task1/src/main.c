38ea13ebd0c72d102d2c89bcb37bee74f06dc071
launaitm <launatim@student.21-school.ru>
2024-04-12 19:33:40 +0300

    обработка ошибок по таске :clown:

--- task1/src/main.c
+++ task1/src/main.c
@@ -6,11 +6,15 @@ int main() {
   matrix_t A, B, C;
   scanf("%d", &n);
   if (n < 1) {
-    printf("Matrix demension must be > 0: %d", n);
+    printf("n/a");
     return 1;
   }
   create_matrix(n, n, &A);
   create_matrix(n, n, &B);
+  if (IsCorr(&A) != kOk || IsCorr(&B) != kOk) {
+    printf("n/a");
+    return 1;
+  }
   read_matrix(&A);
   read_matrix(&B);
   mult_matrix(&A, &B, &C);
